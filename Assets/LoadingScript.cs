88352348db56c0ebb824ecba6aa20e9e553f5f55
davishoang96 <davishoang96@gmail.com>
2018-02-23 19:44:47 +0700

    Initial commit

---
+++ Assets/LoadingScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour {
+
+    public Button btnStart;
+    public Button btnSetting;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void checkbtn()
+    {
+        if(btnStart.onClick != null ){
+            loadthescene("scnStart");
+        } else if(btnSetting.onClick != null){
+            loadthescene("scnMenu");
+        }
+    }
+
+    public void loadthescene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    
+}
